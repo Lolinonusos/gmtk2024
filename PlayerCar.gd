9a90fbd61a5ba4b9aac69c61229e1a343bd89e10
Lolinonusos <linus.nordbakken@gmail.com>
2024-08-16 22:28:03 +0200

    Hello

---
+++ PlayerCar.gd
@@ -0,0 +1,13 @@
+extends VehicleBody3D
+
+
+var max_rpm = 500
+var max_torque = 200
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	steering = lerp(steering, Input.get_axis("ui_right", "ui_left") * 0.4, 5 * delta)
+	engine_force = Input.get_axis("ui_down", "ui_up") * 100
